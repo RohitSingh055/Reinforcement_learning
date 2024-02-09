b888c98ea9acb5b219913983a7d058250c979ab9
Rohit Singh <rohitrsn055@gmail.com>
2024-02-09 22:46:50 +0530

    Delete generate_routes.bat

--- generate_routes.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rem Run duarouter command to generate initial route file
-duarouter -n left_network.net.xml --route-files trip.trips.xml -o route.rou.xml --ignore-errors
-
-rem Run Python script to modify the route file with random depart times in sorted order
-python modify_routes.py
-
-echo Script completed.
